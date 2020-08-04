be544b120ac28d14a82f9024bdc2543ebb214b20
Jorge1255 <enriquejorge1255@gmail.com>
2020-08-04 16:12:10 -0500

    feat: Prepare for deploy.

---
+++ docs/precache-manifest.06aab005649ebfa4bc7c522152e77bc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bbd805bb94150d5ba0cad2a59c44600",
+    "url": "/index.html"
+  },
+  {
+    "revision": "707c341ec723a395d56b",
+    "url": "/static/css/main.7a889623.chunk.css"
+  },
+  {
+    "revision": "6b1578778fd38a87f0cb",
+    "url": "/static/js/2.a685c4d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a685c4d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "707c341ec723a395d56b",
+    "url": "/static/js/main.d023dd74.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
